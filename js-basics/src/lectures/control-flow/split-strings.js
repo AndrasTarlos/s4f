f387ad7c5931037f8e13f338a4a0c0d90893d1bc
Daniel Fahrni <daniel.fahrni@bzz.ch>
2021-11-22 21:06:48 +0100

    Kata "Split Strings" added

---
+++ js-basics/src/lectures/control-flow/split-strings.js
@@ -0,0 +1,14 @@
+/**
+ * Lektion 3 - Kontrollfluss:
+ * 6kyu | Split Strings
+ * https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/javascript
+ */
+
+function solution(str) {
+    // TODO
+}
+
+console.log(solution("abcdef"));  // ["ab", "cd", "ef"]
+console.log(solution("abcdefg")); // ["ab", "cd", "ef", "g_"]
+console.log(solution(""));        // []
+console.log(solution());          // []
